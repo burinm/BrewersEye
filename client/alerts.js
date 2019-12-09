96040be0e7dff4d04bb0472f41e19dcfbe38f564
burin <21989631+burinm@users.noreply.github.com>
2019-12-09 10:57:23 -0700

    Dynamically attach sliders to values

--- client/alerts.js
+++ client/alerts.js
@@ -4,31 +4,31 @@
 let alertsList = [
                 {
                     on: false,
-                    id: 'maxAmbientTemperatureAlert',
+                    id: 'maxAmbientTemperature',
                     desc: "Ambient temperature rises over:",
                     value: null
                 },
                 {
                     on: false,
-                    id: 'minAmbientTemperatureAlert',
+                    id: 'minAmbientTemperature',
                     desc: "Ambient temperature falls below:",
                     value: null
                 },
                 {
                     on: false,
-                    id: 'maxFermenterTemperatureAlert',
+                    id: 'maxFermenterTemperature',
                     desc: "Fermenter temperature rises above:",
                     value: null
                 },
                 {
                     on: false,
-                    id: 'minFermenterTemperatureAlert',
+                    id: 'minFermenterTemperature',
                     desc: "Fermenter temperature falls below:",
                     value: null
                 },
                 {
                     on: false,
-                    id: 'maxBubblesAlert',
+                    id: 'maxBubbles',
                     desc: "Bubbles per minute (bpm) rise above:",
                     value: null
                 },
@@ -55,11 +55,11 @@ alertsList.forEach(function(entry) {
 
             html += "<div style='float:left; width:5%'>";
                 let value = 0;
-                html += "<p style='font-size: 20px; margin:0;'>" + value + "</p>";
+                html += "<p id ='" + entry.id + "Value" + "' style='font-size: 20px; margin:0;'>" + value + "</p>";
             html += "</div>";
 
             html += "<div style='float:left; width:50%'>";
-                html += "<input id='maxAmbientTemperature' type='range' min='0' max='100' style='width:100%;' class='slider'>";
+                html += "<input id='" + entry.id + "Slider" + "' type='range' min='0' max='100' style='width:100%;' class='slider'>";
             html += "</div>";
 
             html += "<div class='clear_formatting'></div-->";
@@ -67,8 +67,22 @@ alertsList.forEach(function(entry) {
 });
 
 alertsContainer.innerHTML=html;
+
+let elementSliders = [];
+let elementValues = [];
+
+alertsList.forEach(function(entry, index) {
+    elementSliders[index] = document.getElementById(entry.id + "Slider");
+    elementValues[index]= document.getElementById(entry.id + "Value");
+
+    console.log(entry.id + "Slider");
+    elementSliders[index].onclick = function(value) {
+        elementValues[index].innerHTML = elementSliders[index].value;
+    }
+});
+
 /*
-<!--Alerts-->
+<!--Template-->
         <p>
             <div style="float:left; width:5%">
                 <input id="maxAmbientTemperatureAlert" type="checkbox">          
